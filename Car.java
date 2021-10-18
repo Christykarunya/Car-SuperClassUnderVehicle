07684d7b561f48771f0964b776409d43e61b52f7
Christy C <91734372+Christykarunya@users.noreply.github.com>
2021-10-18 09:31:55 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class Car extends Vehicle {
+	
+	int WheelCar = 4;
+	
+	public void closeDoor() {
+		System.out.println("Close the Door");
+	}
+
+	public void enableAirbag() {
+		System.out.println("Airbag Enabled");
+	}
+}
